3fd49e00c355279cce09b72e35193e1a7282386b
Jake Lishman <jakelishman@gmail.com>
2016-03-08 15:52:31 +0000

    Fix bugs in scf-moments

--- util/scf-moments.c
+++ util/scf-moments.c
@@ -2,16 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define TAILBYTES 10700
-#define BUFSIZE   2048
-#define NSITES    4
+#define TAILBYTES   10700
+#define BUFSIZE     2048
+#define NSITES      4
+#define SEARCHDIST  256
+#define DATASETSIZE 64
 
 int
 main (int argc, char **argv)
 {
     char buf[BUFSIZE];
+    char dataset[DATASETSIZE];
     double mag;
-    char *cur;
+    char *cur, *prev = buf;
 
     if (argc != 2) {
         fprintf (stderr, "Usage: scf-moments dataset.out\n");
@@ -24,6 +27,21 @@ main (int argc, char **argv)
         return EXIT_FAILURE;
     }
 
+    strncpy (dataset, argv[1], DATASETSIZE - 1);
+    dataset[DATASETSIZE - 1] = '\0';
+
+    /* Remove trailing .out */
+    dataset[strlen(dataset) - 4] = '\0';
+
+    /* Change to CSV. */
+    cur = dataset;
+    while (*cur) {
+        if (*cur == '-')
+            *cur = ',';
+        cur++;
+    }
+
+    fputs (dataset, stdout);
 
     /* Read in the tail-ed part of the file into the buffer. */
     fseek (in, -TAILBYTES, SEEK_END);
@@ -31,32 +49,20 @@ main (int argc, char **argv)
     /* Make sure to null-terminate. */
     buf[BUFSIZE - 1] = '\0';
 
-    cur = argv[1];
-    while (*cur) {
-        char out;
-        if (*cur == '-') {
-            out  = ',';
-        } else if (*cur == '.') {
-            putc ('\0', stdin);
-            break;
-        } else {
-            out = *cur;
-        }
-        putc (out, stdin);
-        cur++;
-    }
-
     for (int i = 0; i < NSITES; i++) {
-        char *prev = buf;
         cur = strstr (prev, "sum");
         if (!cur) {
             fprintf (stderr, "Couldn't find enough sites in '%s'.\n", argv[1]);
             return EXIT_FAILURE;
         }
-        sscanf (cur, "sum, %*f %*f %*f %lf", &mag);
+        if (sscanf (cur, "sum %*f %*f %*f %lf", &mag) != 1) {
+            fprintf (stderr, "Error getting magnetisation for site %d in '%s'.\n", i+1, argv[1]);
+            return EXIT_FAILURE;
+        }
         printf (",%.15g", mag);
+        prev += SEARCHDIST;
     }
-    putc ('\n', stdin);
+    putchar ('\n');
 
     fclose (in);
     return EXIT_SUCCESS;
