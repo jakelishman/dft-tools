7e2dda656dc5c0786812851bf904e0e3674776d7
Jake Lishman <jakelishman@gmail.com>
2016-03-11 02:39:48 +0000

    Update scf-moments to pull out band moments

--- util/scf-moments.c
+++ util/scf-moments.c
@@ -13,7 +13,7 @@ main (int argc, char **argv)
 {
     char buf[BUFSIZE];
     char dataset[DATASETSIZE];
-    double mag;
+    double s,d,p,tot;
     char *cur, *prev = buf;
 
     if (argc != 2) {
@@ -50,13 +50,18 @@ main (int argc, char **argv)
     buf[BUFSIZE - 1] = '\0';
 
     for (int i = 0; i < NSITES; i++) {
-        cur = strstr (prev, "sum");
+        cur = strstr (prev, "B_core\n");
         if (!cur) {
             fprintf (stderr, "Couldn't find enough sites in '%s'.\n", argv[1]);
             return EXIT_FAILURE;
         }
-        sscanf (cur, "sum %*f %*f %*f %lf", &mag);
-        printf (",%.15g", mag);
+        sscanf (cur, "B_core\n"
+                     " s  %*f %*f %*f %lf %*f %*f %*f s %*f\n"
+                     " p  %*f %*f %*f %lf %*f %*f %*f ns %*f\n"
+                     " d  %*f %*f %*f %lf %*f %*f %*f cor %*f\n"
+                     "sum %*f %*f %*f %lf %*f %*f %*f v+c %*f\n",
+                     &s, &p, &d, &tot);
+        printf (",%.15g,%.15g,%.15g,%.15g", s, p, d, tot);
         prev = cur + SEARCHDIST;
     }
     putchar ('\n');
